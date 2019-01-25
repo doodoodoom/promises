bcc800c25a781104283d703d90a5aa725d15edfa
doodoodoom <madridmatt17@gmail.com>
2019-01-24 20:47:28 -0800

    Implement basicChaining

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,13 +8,38 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
-var fs = require('fs');
-var Promise = require('bluebird');
+//promisification
 
+// module.exports = {
+//   getGitHubProfileAsync: getGitHubProfileAsync,
+//   generateRandomTokenAsync: generateRandomTokenAsync,
+//   readFileAndMakeItFunnyAsync: readFileAndMakeItFunnyAsync
+// };
+
+/*
+  promiseConstructor
+
+  module.exports = {
+  getStatusCodeAsync: getStatusCodeAsync,
+  pluckFirstLineFromFileAsync: pluckFirstLineFromFileAsync
+
+};
+*/
+
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var promisification = require('./promisification.js'); 
+var promiseConstructor = require('./promiseConstructor.js'); 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return promiseConstructor.pluckFirstLineFromFileAsync(readFilePath)
+    .then(function(user) {
+      return promisification.getGitHubProfileAsync(user);
+    })
+    .then(function(userProfile){
+      return fs.writeFileAsync(writeFilePath, JSON.stringify(userProfile));
+    });
 };
 
 // Export these functions so we can test them
