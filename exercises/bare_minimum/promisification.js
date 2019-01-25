588c12ee39d5b4e79df87e7c3dec22839b894ae1
doodoodoom <madridmatt17@gmail.com>
2019-01-24 19:37:15 -0800

    Implement promisification

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -27,7 +27,11 @@ var getGitHubProfile = function(user, callback) {
   });
 };
 
-var getGitHubProfileAsync; // TODO
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile); // TODO
+
+// var nodeStyle = require('./callbackReview.js');
+// var pluckFirstLineFromFileAsync = Promise.promisify(nodeStyle.pluckFirstLineFromFile)
+// var getStatusCodeAsync = Promise.promisify(nodeStyle.getStatusCode)
 
 
 // (2) Asyncronous token generation
@@ -38,13 +42,13 @@ var generateRandomToken = function(callback) {
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken); // TODO
 
 
 // (3) Asyncronous file manipulation
 var readFileAndMakeItFunny = function(filePath, callback) {
   fs.readFile(filePath, 'utf8', function(err, file) {
-    if (err) { return callback(err); }
+    if (err) { return callback(err, file); }
    
     var funnyFile = file.split('\n')
       .map(function(line) {
@@ -52,11 +56,11 @@ var readFileAndMakeItFunny = function(filePath, callback) {
       })
       .join('\n');
 
-    callback(funnyFile);
+    callback(err, funnyFile);
   });
 };
 
-var readFileAndMakeItFunnyAsync; // TODO
+var readFileAndMakeItFunnyAsync = Promise.promisify(readFileAndMakeItFunny); // TODO
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
